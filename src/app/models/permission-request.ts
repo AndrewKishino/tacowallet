afedd5a072abf4e52e922585d27b88c6e22789a8
Simon Boissonneault-Robert <simrob7000@gmail.com>
2020-03-16 22:42:02 +0000

    Update gh-pages

--- src/app/models/permission-request.ts
+++
@@ -1,7 +0,0 @@
-export type PermissionScope = "read_address" | "sign" | "payment_request" | "threshold";
-
-export class PermissionRequest {
-  constructor(public readonly scope: PermissionScope, public appID: string) {
-
-  }
-}
